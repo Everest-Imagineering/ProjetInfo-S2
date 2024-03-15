bc45450632c5d886c6f41fbe61b2c645e80e9b70
Everest-Imagineering <elmx327@gmail.com>
2024-03-15 08:48:49 +0100

    etat initial

---
+++ src/main/java/fr/insa/marx/projetbatiment/Mur.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.marx.projetbatiment;
+
+/**
+ *
+ * @author emarx02
+ */
+public class Mur {
+    
+    private int id, rev1, rev2, rev3, portes, fenetres;
+    private double separation;
+    private Coin pt1, pt2;
+
+    public Mur(Coin pt1, Coin pt2) {
+        this.pt1 = pt1;
+        this.pt2 = pt2;    }
+    
+    public double Longueur(){
+        double longueur;
+        longueur = Math.sqrt((this.pt1.getX()-this.pt2.getX())*(this.pt1.getX()-this.pt2.getX()) + (this.pt1.getY()-this.pt2.getY())*(this.pt1.getY()-this.pt2.getY()));
+    return longueur;
+    }
+
+
+    
+    
+}
